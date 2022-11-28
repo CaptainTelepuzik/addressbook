0277148b7ade61b704fba77ef3a4d870e1e6abd2
CaptainTelepuzik <giztimur@yandex.ru>
2022-11-28 23:53:05 +0500

    28.11

--- classes/Contact.py
+++ classes/Contact.py
@@ -7,3 +7,14 @@ class Contact(BaseClass):
     def get_model(new_model: bool = False) -> ContactModel:
         return ContactModel() if new_model else ContactModel
 
+    def _prepare_query_filter(self, query, filter_params):
+        """
+         Применение фильтрации к объекту записи
+        :param query: Объект запроса
+        :param filter_params: Фильтр
+        :return: Объект запроса с фильтрацией
+        """
+        if filter_params.get('X-User_ID'):
+            query = query.where(self.get_model().user_id == filter_params.get('X-User_ID'))
+
+        return query
